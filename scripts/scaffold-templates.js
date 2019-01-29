35e9308e568b53f6770b41a23879f8fa7c7d568d
Anastasiya Flynn <anastasiyar29@gmail.com>
2019-01-29 18:31:47 -0500

    whitespace

--- scripts/scaffold-templates.js
+++ scripts/scaffold-templates.js
@@ -1,11 +1,11 @@
 module.exports.getTemplate = (componentName, template) => {
-    const exportVarName = componentName.replace(/[^\w]+/g, '');
-    const templates = {
+  const exportVarName = componentName.replace(/[^\w]+/g, '');
+  const templates = {
 
-        // Template for React Class Component
-        rcc: `import React, { Component } from 'react';
+    // Template for React Class Component
+    rcc: `import React, { Component } from 'react';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
-        
+
 export default class ${exportVarName} extends Component {
   render() {
     return <div>
@@ -16,25 +16,25 @@ export default class ${exportVarName} extends Component {
   };
 `,
 
-        // Template for React Functional Component
-        rfc: `import React from 'react';
+    // Template for React Functional Component
+    rfc: `import React from 'react';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
-        
+
 const ${exportVarName} = (props) => (
   <div>
     <p>${componentName} Component</p>
     <Text field={props.fields.heading} />
   </div>
 );
-        
+
 export default ${exportVarName};
 `
-    };
+  };
 
-    if (templates.hasOwnProperty(template)) {
-        return templates[template]
-    }
+  if (templates.hasOwnProperty(template)) {
+    return templates[template]
+  }
 
-    console.log('No component template or invalid template specified, using React Functional Component template.');
-    return templates.rfc;
+  console.log('No component template or invalid template specified, using React Functional Component template.');
+  return templates.rfc;
 };
